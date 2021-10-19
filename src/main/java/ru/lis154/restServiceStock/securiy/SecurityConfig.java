505fc8aba8867e83963d2859867940fc43755811
lis154 <lis154@mail.ru>
2021-10-19 22:43:53 +0300

    commit 1

--- src/main/java/ru/lis154/restServiceStock/securiy/SecurityConfig.java
+++ src/main/java/ru/lis154/restServiceStock/securiy/SecurityConfig.java
@@ -3,10 +3,11 @@ package ru.lis154.restServiceStock.securiy;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
